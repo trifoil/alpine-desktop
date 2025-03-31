21d5222734106f2a092b45f66b127fdf73be202d
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2025-03-31 17:37:36 +0200

    hmmmm

--- install.sh
+++ install.sh
@@ -1,28 +1,37 @@
 #!/bin/bash
 
-# Update and upgrade
+# Update and upgrade system
 apk update && apk upgrade
 
-# Add community repo (if not already added)
+# Add community repository if not already added
 if ! grep -q '^http://dl-cdn.alpinelinux.org/alpine/latest-stable/community' /etc/apk/repositories; then
     echo "http://dl-cdn.alpinelinux.org/alpine/latest-stable/community" >> /etc/apk/repositories
 fi
 
-setup-xorg-base
-
+# Install Xorg and GNOME
+apk add setup-xorg-base
 apk add gnome gnome-apps-core gnome-apps-extra
 
+# Install necessary dependencies
+apk add dbus elogind polkit bash bash-completion thunar-volman
+
+# Enable and start required services
+rc-update add dbus default
+rc-service dbus start
+
+rc-update add elogind default
+rc-service elogind start
+
+rc-update add gdm default
 rc-service gdm start
 
-rc-update add gdm
-
-apk add bash bash-completion
-
-apk add thunar-volman
-
-apk update && apk upgrade
-
+# Ensure user exists and is in the correct groups
 adduser x -h /home/x
+adduser x video
+adduser x audio
+adduser x input
+adduser x seat
 
+# Final system update and reboot
+apk update && apk upgrade
 reboot
-
