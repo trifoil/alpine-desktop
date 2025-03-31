2b075a6e2ef79ec9b31f0a13def394cdfbf2a507
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2025-03-31 18:24:36 +0200

    a

--- install.sh
+++ install.sh
@@ -4,40 +4,36 @@
 apk update && apk upgrade
 
 # Add main and community repositories if not already added
-if ! grep -q '^http://dl-cdn.alpinelinux.org/alpine/latest-stable/main' /etc/apk/repositories; then
-    echo "http://dl-cdn.alpinelinux.org/alpine/latest-stable/main" >> /etc/apk/repositories
+if ! grep -q '^http://dl-cdn.alpinelinux.org/alpine/edge/main' /etc/apk/repositories; then
+    echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories
 fi
 
-if ! grep -q '^http://dl-cdn.alpinelinux.org/alpine/latest-stable/community' /etc/apk/repositories; then
-    echo "http://dl-cdn.alpinelinux.org/alpine/latest-stable/community" >> /etc/apk/repositories
+if ! grep -q '^http://dl-cdn.alpinelinux.org/alpine/edge/community' /etc/apk/repositories; then
+    echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
+fi
+
+if ! grep -q '^http://dl-cdn.alpinelinux.org/alpine/edge/testing' /etc/apk/repositories; then
+    echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
 fi
 
 # Update package lists
 apk update && apk upgrade
 
 # Install Xorg and GNOME
-apk add xorg-server xf86-video-vesa xf86-input-libinput
-apk add gnome gnome-apps-core
+setup-xorg-base 
+apk add gnome
+apk add gnome-apps-core
+apk add gnome-apps-extra
 
-# Install necessary dependencies
-apk add dbus elogind polkit bash bash-completion thunar-volman
-
-# Enable and start required services
-rc-update add dbus default
-rc-service dbus start
-
-rc-update add elogind default
-rc-service elogind start
-
-rc-update add gdm default
 rc-service gdm start
+rc-update add gdm
+apk add bash
+apk add bash-completion
+apk add thunar-volman
+apk update
 
-# Ensure user exists and is in the correct groups
 adduser x -h /home/x
-adduser x video
-adduser x audio
-adduser x input
-adduser x seat
+
 
 # Final system update and reboot
 apk update && apk upgrade
