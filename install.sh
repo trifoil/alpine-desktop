804776edc1bb5ebe125b59935060647d6705eead
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2025-03-31 16:20:49 +0200

    hehe

--- install.sh
+++ install.sh
@@ -4,10 +4,33 @@
 apk update && apk upgrade
 
 # Add community repo (if not already added)
-echo "http://dl-cdn.alpinelinux.org/alpine/latest-stable/community" >> /etc/apk/repositories
+if ! grep -q '^http://dl-cdn.alpinelinux.org/alpine/latest-stable/community' /etc/apk/repositories; then
+    echo "http://dl-cdn.alpinelinux.org/alpine/latest-stable/community" >> /etc/apk/repositories
+fi
 
-# Install GNOME
+# Install GNOME desktop (core components only)
 setup-desktop gnome
 
-# Remove useless stuff
-apk del gnome-weather gnome-calendar gnome-music gnome-camera gnome-tour gnome-videos gnome-user-docs gnome-documents simple-scan
+# Now force-remove the unwanted applications
+apk del \
+    gnome-weather \
+    gnome-calendar \
+    gnome-music \
+    gnome-camera \
+    gnome-tour \
+    gnome-videos \
+    gnome-user-docs \
+    gnome-documents \
+    simple-scan
+
+# Optional: Mark these packages as explicitly unwanted to prevent reinstallation
+apk add --virtual .unwanted-gnome-apps \
+    gnome-weather \
+    gnome-calendar \
+    gnome-music \
+    gnome-camera \
+    gnome-tour \
+    gnome-videos \
+    gnome-user-docs \
+    gnome-documents \
+    simple-scan
