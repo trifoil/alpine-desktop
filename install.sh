81d5066f3c60178e13596528ae01cf9b7ed95897
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2025-03-31 20:49:11 +0200

    test

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Exit on error and print commands
-set -e
+set -ex
 
 # Check if running as root
 if [ "$(id -u)" -ne 0 ]; then
@@ -9,62 +9,6 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-# Make the cleanup script executable
-chmod +x /etc/post-install-cleanup.sh
+setup-desktop gnome
 
-# Create autostart entry for the cleanup script
-cat > /etc/local.d/post-install-cleanup.start << 'EOL'
-#!/bin/sh
-/etc/post-install-cleanup.sh && rc-service local stop
-EOL
-
-chmod +x /etc/local.d/post-install-cleanup.start
-
-# Update and upgrade system
-echo "Updating system packages..."
-apk update && apk upgrade
-
-# Configure repositories
-echo "Configuring repositories..."
-if ! grep -q '^http://dl-cdn.alpinelinux.org/alpine/edge/main' /etc/apk/repositories; then
-    echo "http://dl-cdn.alpinelinux.org/alpine/edge/main" >> /etc/apk/repositories
-fi
-
-if ! grep -q '^http://dl-cdn.alpinelinux.org/alpine/edge/community' /etc/apk/repositories; then
-    echo "http://dl-cdn.alpinelinux.org/alpine/edge/community" >> /etc/apk/repositories
-fi
-
-if ! grep -q '^http://dl-cdn.alpinelinux.org/alpine/edge/testing' /etc/apk/repositories; then
-    echo "http://dl-cdn.alpinelinux.org/alpine/edge/testing" >> /etc/apk/repositories
-fi
-
-# Update package lists
-echo "Updating package lists..."
-apk update && apk upgrade
-
-# Install Xorg and complete GNOME
-echo "Installing Xorg and GNOME..."
-setup-xorg-base
-apk add gnome gnome-apps-core 
-
-# Install additional required packages
-echo "Installing additional packages..."
-apk add bash bash-completion 
-
-# Create user if not exists
-if ! id -u x >/dev/null 2>&1; then
-    echo "Creating user 'x'..."
-    adduser -D -h /home/x x
-fi
-
-# Enable services (but don't start gdm yet to prevent desktop loading before cleanup)
-rc-update add gdm
-rc-update add local default
-
-# Final system update
-echo "Performing final updates..."
-apk update && apk upgrade
-
-echo "Installation complete! The system will now reboot..."
-echo "After reboot, the unwanted applications will be automatically removed."
 reboot
