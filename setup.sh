68ab8136c93ab42af7165d98b320f5c65637d21e
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2025-04-01 17:35:02 +0200

    a

--- setup.sh
+++ setup.sh
@@ -6,16 +6,19 @@ display_menu() {
         clear
         echo "Select a script to execute:" 
         echo "---------------------------------"
-        local i=1
-        declare -A script_map
         
         # Find .sh files and sort them
-        for script in $(ls *.sh 2>/dev/null | grep -v "setup.sh" | sort -V); do
-            script_name="${script%.sh}"
-            script_map[$i]="$script"
-            echo "$i) $script_name"
-            ((i++))
-        done
+        scripts=($(ls *.sh 2>/dev/null | grep -v "setup.sh" | sort -V))
+        script_count=${#scripts[@]}
+        
+        if [ $script_count -eq 0 ]; then
+            echo "No scripts found."
+        else
+            for i in "${!scripts[@]}"; do
+                script_name="${scripts[$i]%.sh}"
+                echo "$((i + 1))) $script_name"
+            done
+        fi
         
         echo "q) Quit"
         echo "---------------------------------"
@@ -24,10 +27,11 @@ display_menu() {
         if [[ "$choice" == "q" ]]; then
             echo "Exiting..."
             exit 0
-        elif [[ -n "${script_map[$choice]}" ]]; then
-            echo "Executing ${script_map[$choice]}..."
-            chmod +x "${script_map[$choice]}"
-            "./${script_map[$choice]}"
+        elif [[ "$choice" =~ ^[0-9]+$ ]] && (( choice >= 1 && choice <= script_count )); then
+            script="${scripts[$((choice - 1))]}"
+            echo "Executing $script..."
+            chmod +x "$script"
+            "./$script"
             read -p "Press Enter to continue..."
         else
             echo "Invalid choice! Try again."
