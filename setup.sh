05fff3245b049c24b7b12a1459efde297249f0d1
I͍͍̪̖̥͎̺͈̓ͯͨ̄̌̐̍̍ a͊ͦ̇ͤ̾m̴̴̸̵̧̡̛̞͔̮̖͉̝̥͔̟̺̼̟̈́̄̔ͦ̅͐ͦ̑͗ͬͮ́ͯ̓͘ a̓ d̪̜͗̀ͩ̐̽ͫ̃ͮ̽̃͢͞ǒ̘̤͐̇̃͒͝ͅr̋į̸͓̠ͤ́̋̓͊̆͋̄ͧ̒͆̚̕t̡͔͐̏ͅơ̩͍͍̯̮̪̰̯̟̻̙̯̋̊͌͑ͯͤͣ̓̎͗͒́͛̑̚͢ <143878567+trifoil@users.noreply.github.com>
2025-04-01 17:41:54 +0200

    hehe

--- setup.sh
+++ setup.sh
@@ -1,43 +1,15 @@
-#!/bin/bash
+options=(
+    "Basic install"
+    "Latex full install"
+    "Quit" 
+)
 
-# Function to display menu and execute scripts
-display_menu() {
-    while true; do
-        clear
-        echo "Select a script to execute:" 
-        echo "---------------------------------"
-        
-        # Find .sh files and sort them
-        scripts=($(ls *.sh 2>/dev/null | grep -v "setup.sh" | sort -V))
-        script_count=${#scripts[@]}
-        
-        if [ $script_count -eq 0 ]; then
-            echo "No scripts found."
-        else
-            for i in "${!scripts[@]}"; do
-                script_name="${scripts[$i]%.sh}"
-                echo "$((i + 1))) $script_name"
-            done
-        fi
-        
-        echo "q) Quit"
-        echo "---------------------------------"
-        read -p "Enter your choice: " choice
+PS3="Enter a number (1-${#options[@]}): "
 
-        if [[ "$choice" == "q" ]]; then
-            echo "Exiting..."
-            exit 0
-        elif [[ "$choice" =~ ^[0-9]+$ ]] && (( choice >= 1 && choice <= script_count )); then
-            script="${scripts[$((choice - 1))]}"
-            echo "Executing $script..."
-            chmod +x "$script"
-            "./$script"
-            read -p "Press Enter to continue..."
-        else
-            echo "Invalid choice! Try again."
-            sleep 1
-        fi
-    done
-}
-
-display_menu
+select option in "${options[@]}"; do
+    case "$REPLY" in 
+        1) ./1_basic_install.sh ;;
+        2) ./2_latex_install.sh ;;
+        q) break ;;
+    esac
+done
